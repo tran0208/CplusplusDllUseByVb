bf634c70337c63052ffa7916417a791c0d45f44d
Minh Tran <minh.t.tran@adelaide.edu.au>
2018-10-03 14:44:14 +0930

    apdate

--- Form/Form.vb
+++ Form/Form.vb
@@ -5,24 +5,27 @@ Public Class Form
 
     <DllImport("dllTest.dll", CallingConvention:=CallingConvention.Cdecl)> _
     Public Shared Function HelloWorld(ByVal x As Int32) As Int32
-    End Function
-
+    End Function 
     Private Declare Function HelloWorld Lib "dllTest.dll" (int) As Integer
 
 
 
-    ' <DllImport("Math.dll", CallingConvention:= CallingConvention.Cdecl)> _
-    ' Public Shared Function HelloWorld( ByVal x As Int32) As Int32 
-    ' End Function
 
-    'Private Declare Function HelloWorld Lib "Math.dll" (int) As Integer
+    <DllImport("dllTest.dll", CallingConvention:=CallingConvention.Cdecl)> _
+    Public Shared Function compute(ByVal x As double) As double
+    End Function 
+    Private Declare Function compute Lib "dllTest.dll" (float) As double
 
 
+
+
+ 
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim j
 
-        j = HelloWorld(CInt(TextBox1.Text))
+        'j = HelloWorld(CInt(TextBox1.Text))
+        j = compute(Convert.ToDouble( "0" + TextBox1.Text ))
         TextBox2.Text = j
         
     End Sub
